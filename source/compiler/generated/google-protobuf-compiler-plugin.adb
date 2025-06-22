94bfb858b72a4e2340b4b6b5a4a76d06f301ff37
Maxim Reznik <reznikmm@gmail.com>
2025-06-22 18:43:51 +0300

    Regenerate compiler plugin and descriptor API
    from v31.1 (rather than v3.1.0).
    
    To avoid name clash on `declaration` apply this patch:
    
    ```diff
    --- descriptor.proto_	2025-06-22 16:02:44.840209235 +0300
    +++ descriptor.proto	2025-06-22 16:03:13.479327421 +0300
    @@ -225,7 +225,7 @@
       // The verification state of the extension range.
       enum VerificationState {
         // All the extensions of the range must be declared.
    -    DECLARATION = 0;
    +    EXTENSION_DECLARATION = 0;
         UNVERIFIED = 1;
       }
    
    ```

--- source/compiler/generated/google-protobuf-compiler-plugin.adb
+++ source/compiler/generated/google-protobuf-compiler-plugin.adb
@@ -4,17 +4,158 @@ with PB_Support.Internal;
 
 package body Google.Protobuf.Compiler.Plugin is
 
-   package File_Descriptor_Proto_IO is
+   package Google_Protobuf_Descriptor_File_Descriptor_Proto_IO is
      new PB_Support.IO.Message_IO
        (Google.Protobuf.Descriptor.File_Descriptor_Proto,
         Google.Protobuf.Descriptor.File_Descriptor_Proto_Vector,
         Google.Protobuf.Descriptor.Append);
 
-   package File_IO is
+   package Google_Protobuf_Descriptor_Generated_Code_Info_IO is
+     new PB_Support.IO.Message_IO
+       (Google.Protobuf.Descriptor.Generated_Code_Info,
+        Google.Protobuf.Descriptor.Generated_Code_Info_Vector,
+        Google.Protobuf.Descriptor.Append);
+
+   package Google_Protobuf_Compiler_Plugin_File_IO is
      new PB_Support.IO.Message_IO
        (Google.Protobuf.Compiler.Plugin.File,
         Google.Protobuf.Compiler.Plugin.File_Vector,
         Google.Protobuf.Compiler.Plugin.Append);
+
+   package Google_Protobuf_Compiler_Plugin_Version_IO is
+     new PB_Support.IO.Message_IO
+       (Google.Protobuf.Compiler.Plugin.Version,
+        Google.Protobuf.Compiler.Plugin.Version_Vector,
+        Google.Protobuf.Compiler.Plugin.Append);
+
+   function Length (Self : Version_Vector) return Natural is
+   begin
+      return Self.Length;
+   end Length;
+
+   procedure Clear (Self : in out Version_Vector) is
+   begin
+      Self.Length := 0;
+   end Clear;
+
+   procedure Free is new Ada.Unchecked_Deallocation
+     (Version_Array, Version_Array_Access);
+
+   procedure Append (Self : in out Version_Vector; V    : Version) is
+      Init_Length : constant Positive := Positive'Max (1, 256 / Version'Size);
+      Aux_Data    : Version_Array_Access;
+   begin
+      if Self.Length = 0 then
+         Self.Data :=  new Version_Array (1 .. Init_Length);
+
+      elsif Self.Length = Self.Data'Last then
+         Aux_Data := Self.Data;
+         Self.Data :=
+           new Version_Array'
+             (Self.Data.all & Version_Array'(1 .. Self.Length => <>));
+         Free (Aux_Data);
+      end if;
+      Self.Length := Self.Length + 1;
+      Self.Data (Self.Length) := V;
+   end Append;
+
+   overriding procedure Adjust (Self : in out Version_Vector) is
+   begin
+      if Self.Length > 0 then
+         Self.Data := new Version_Array'(Self.Data (1 .. Self.Length));
+      end if;
+   end Adjust;
+
+   overriding procedure Finalize (Self : in out Version_Vector) is
+   begin
+      if Self.Data /= null then
+         Free (Self.Data);
+      end if;
+   end Finalize;
+
+   not overriding function Get_Version_Variable_Reference
+    (Self  : aliased in out Version_Vector;
+     Index : Positive)
+      return Version_Variable_Reference is
+   begin
+      return (Element => Self.Data (Index)'Access);
+   end Get_Version_Variable_Reference;
+
+   not overriding function Get_Version_Constant_Reference
+    (Self  : aliased Version_Vector;
+     Index : Positive)
+      return Version_Constant_Reference is
+   begin
+      return (Element => Self.Data (Index)'Access);
+   end Get_Version_Constant_Reference;
+
+   procedure Read_Version
+    (Stream : access Ada.Streams.Root_Stream_Type'Class;
+     V      : out Version) is
+      Key : aliased PB_Support.IO.Key;
+   begin
+      while PB_Support.IO.Read_Key (Stream, Key'Access) loop
+         case Key.Field is
+            when 1 =>
+               if  not V.Major.Is_Set then
+                  V.Major := (True, others => <>);
+               end if;
+               PB_Support.IO.Read_Varint (Stream, Key.Encoding, V.Major.Value);
+            when 2 =>
+               if  not V.Minor.Is_Set then
+                  V.Minor := (True, others => <>);
+               end if;
+               PB_Support.IO.Read_Varint (Stream, Key.Encoding, V.Minor.Value);
+            when 3 =>
+               if  not V.Patch.Is_Set then
+                  V.Patch := (True, others => <>);
+               end if;
+               PB_Support.IO.Read_Varint (Stream, Key.Encoding, V.Patch.Value);
+            when 4 =>
+               if  not V.Suffix.Is_Set then
+                  V.Suffix := (True, others => <>);
+               end if;
+               PB_Support.IO.Read (Stream, Key.Encoding, V.Suffix.Value);
+            when others =>
+               PB_Support.IO.Unknown_Field (Stream, Key.Encoding);
+         end case;
+      end loop;
+   end Read_Version;
+
+   procedure Write_Version
+    (Stream : access Ada.Streams.Root_Stream_Type'Class;
+     V      : Version) is
+   begin
+      if Stream.all not in PB_Support.Internal.Stream then
+         declare
+            WS : aliased PB_Support.Internal.Stream (Stream);
+         begin
+            Write_Version (WS'Access, V);
+            return;
+         end;
+      end if;
+      declare
+         WS : PB_Support.Internal.Stream renames
+           PB_Support.Internal.Stream (Stream.all);
+      begin
+         WS.Start_Message;
+         if V.Major.Is_Set then
+            WS.Write_Varint (1, V.Major.Value);
+         end if;
+         if V.Minor.Is_Set then
+            WS.Write_Varint (2, V.Minor.Value);
+         end if;
+         if V.Patch.Is_Set then
+            WS.Write_Varint (3, V.Patch.Value);
+         end if;
+         if V.Suffix.Is_Set then
+            WS.Write (4, V.Suffix.Value);
+         end if;
+         if WS.End_Message then
+            Write_Version (WS'Access, V);
+         end if;
+      end;
+   end Write_Version;
 
    function Length (Self : Code_Generator_Request_Vector) return Natural is
    begin
@@ -34,15 +175,18 @@ package body Google.Protobuf.Compiler.Plugin is
      V    : Code_Generator_Request) is
       Init_Length : constant Positive :=
         Positive'Max (1, 256 / Code_Generator_Request'Size);
+      Aux_Data    : Code_Generator_Request_Array_Access;
    begin
       if Self.Length = 0 then
          Self.Data :=  new Code_Generator_Request_Array (1 .. Init_Length);
 
       elsif Self.Length = Self.Data'Last then
+         Aux_Data := Self.Data;
          Self.Data :=
            new Code_Generator_Request_Array'
              (Self.Data.all
                 & Code_Generator_Request_Array'(1 .. Self.Length => <>));
+         Free (Aux_Data);
       end if;
       Self.Length := Self.Length + 1;
       Self.Data (Self.Length) := V;
@@ -96,8 +240,17 @@ package body Google.Protobuf.Compiler.Plugin is
                end if;
                PB_Support.IO.Read (Stream, Key.Encoding, V.Parameter.Value);
             when 15 =>
-               File_Descriptor_Proto_IO.Read_Vector
+               Google_Protobuf_Descriptor_File_Descriptor_Proto_IO.Read_Vector
                  (Stream, Key.Encoding, V.Proto_File);
+            when 17 =>
+               Google_Protobuf_Descriptor_File_Descriptor_Proto_IO.Read_Vector
+                 (Stream, Key.Encoding, V.Source_File_Descriptors);
+            when 3 =>
+               if  not V.Compiler_Version.Is_Set then
+                  V.Compiler_Version := (True, others => <>);
+               end if;
+               Google_Protobuf_Compiler_Plugin_Version_IO.Read
+                 (Stream, Key.Encoding, V.Compiler_Version.Value);
             when others =>
                PB_Support.IO.Unknown_Field (Stream, Key.Encoding);
          end case;
@@ -130,6 +283,16 @@ package body Google.Protobuf.Compiler.Plugin is
             Google.Protobuf.Descriptor.File_Descriptor_Proto'Write
               (Stream, V.Proto_File (J));
          end loop;
+         for J in 1 .. V.Source_File_Descriptors.Length loop
+            WS.Write_Key ((17, PB_Support.Length_Delimited));
+            Google.Protobuf.Descriptor.File_Descriptor_Proto'Write
+              (Stream, V.Source_File_Descriptors (J));
+         end loop;
+         if V.Compiler_Version.Is_Set then
+            WS.Write_Key ((3, PB_Support.Length_Delimited));
+            Google.Protobuf.Compiler.Plugin.Version'Write
+              (Stream, V.Compiler_Version.Value);
+         end if;
          if WS.End_Message then
             Write_Code_Generator_Request (WS'Access, V);
          end if;
@@ -154,15 +317,18 @@ package body Google.Protobuf.Compiler.Plugin is
      V    : Code_Generator_Response) is
       Init_Length : constant Positive :=
         Positive'Max (1, 256 / Code_Generator_Response'Size);
+      Aux_Data    : Code_Generator_Response_Array_Access;
    begin
       if Self.Length = 0 then
          Self.Data :=  new Code_Generator_Response_Array (1 .. Init_Length);
 
       elsif Self.Length = Self.Data'Last then
+         Aux_Data := Self.Data;
          Self.Data :=
            new Code_Generator_Response_Array'
              (Self.Data.all
                 & Code_Generator_Response_Array'(1 .. Self.Length => <>));
+         Free (Aux_Data);
       end if;
       Self.Length := Self.Length + 1;
       Self.Data (Self.Length) := V;
@@ -213,8 +379,27 @@ package body Google.Protobuf.Compiler.Plugin is
                   V.Error := (True, others => <>);
                end if;
                PB_Support.IO.Read (Stream, Key.Encoding, V.Error.Value);
+            when 2 =>
+               if  not V.Supported_Features.Is_Set then
+                  V.Supported_Features := (True, others => <>);
+               end if;
+               PB_Support.IO.Read_Varint
+                 (Stream, Key.Encoding, V.Supported_Features.Value);
+            when 3 =>
+               if  not V.Minimum_Edition.Is_Set then
+                  V.Minimum_Edition := (True, others => <>);
+               end if;
+               PB_Support.IO.Read_Varint
+                 (Stream, Key.Encoding, V.Minimum_Edition.Value);
+            when 4 =>
+               if  not V.Maximum_Edition.Is_Set then
+                  V.Maximum_Edition := (True, others => <>);
+               end if;
+               PB_Support.IO.Read_Varint
+                 (Stream, Key.Encoding, V.Maximum_Edition.Value);
             when 15 =>
-               File_IO.Read_Vector (Stream, Key.Encoding, V.File);
+               Google_Protobuf_Compiler_Plugin_File_IO.Read_Vector
+                 (Stream, Key.Encoding, V.File);
             when others =>
                PB_Support.IO.Unknown_Field (Stream, Key.Encoding);
          end case;
@@ -241,6 +426,15 @@ package body Google.Protobuf.Compiler.Plugin is
          if V.Error.Is_Set then
             WS.Write (1, V.Error.Value);
          end if;
+         if V.Supported_Features.Is_Set then
+            WS.Write_Varint (2, V.Supported_Features.Value);
+         end if;
+         if V.Minimum_Edition.Is_Set then
+            WS.Write_Varint (3, V.Minimum_Edition.Value);
+         end if;
+         if V.Maximum_Edition.Is_Set then
+            WS.Write_Varint (4, V.Maximum_Edition.Value);
+         end if;
          for J in 1 .. V.File.Length loop
             WS.Write_Key ((15, PB_Support.Length_Delimited));
             Google.Protobuf.Compiler.Plugin.File'Write (Stream, V.File (J));
@@ -266,14 +460,17 @@ package body Google.Protobuf.Compiler.Plugin is
 
    procedure Append (Self : in out File_Vector; V    : File) is
       Init_Length : constant Positive := Positive'Max (1, 256 / File'Size);
+      Aux_Data    : File_Array_Access;
    begin
       if Self.Length = 0 then
          Self.Data :=  new File_Array (1 .. Init_Length);
 
       elsif Self.Length = Self.Data'Last then
+         Aux_Data := Self.Data;
          Self.Data :=
            new File_Array'
              (Self.Data.all & File_Array'(1 .. Self.Length => <>));
+         Free (Aux_Data);
       end if;
       Self.Length := Self.Length + 1;
       Self.Data (Self.Length) := V;
@@ -332,6 +529,12 @@ package body Google.Protobuf.Compiler.Plugin is
                   V.Content := (True, others => <>);
                end if;
                PB_Support.IO.Read (Stream, Key.Encoding, V.Content.Value);
+            when 16 =>
+               if  not V.Generated_Code_Info.Is_Set then
+                  V.Generated_Code_Info := (True, others => <>);
+               end if;
+               Google_Protobuf_Descriptor_Generated_Code_Info_IO.Read
+                 (Stream, Key.Encoding, V.Generated_Code_Info.Value);
             when others =>
                PB_Support.IO.Unknown_Field (Stream, Key.Encoding);
          end case;
@@ -363,6 +566,11 @@ package body Google.Protobuf.Compiler.Plugin is
          end if;
          if V.Content.Is_Set then
             WS.Write (15, V.Content.Value);
+         end if;
+         if V.Generated_Code_Info.Is_Set then
+            WS.Write_Key ((16, PB_Support.Length_Delimited));
+            Google.Protobuf.Descriptor.Generated_Code_Info'Write
+              (Stream, V.Generated_Code_Info.Value);
          end if;
          if WS.End_Message then
             Write_File (WS'Access, V);
