e69d9de5f5e32628b0782dfee49e763f1a1e51d2
mgrojo <mgrojo@gmail.com>
2026-04-19 00:38:33 +0200

    Add a "DO NOT EDIT" header comment
    Inspired from the cpp generated header comment (and from other code generators):
    
    ```ada
    -- Generated by the protocol buffer compiler. DO NOT EDIT!
    -- Source: bzz_bzz_bzz.proto
    -- Protobuf Compiler Version: X.Y.Z
    ```
    
    - Implements issue #32
    - Generated files refreshed.
    - Also rename project to match filename and split source dirs for readability.

--- source/compiler/compiler-file_descriptors.ads
+++ source/compiler/compiler-file_descriptors.ads
@@ -46,7 +46,8 @@ package Compiler.File_Descriptors is
    --  Return base name for Ada package
 
    function Body_Text
-     (Self : Google.Protobuf.Descriptor.File_Descriptor_Proto)
+     (Self    : Google.Protobuf.Descriptor.File_Descriptor_Proto;
+      Request : Google.Protobuf.Compiler.Plugin.Code_Generator_Request)
       return League.Strings.Universal_String;
 
 end Compiler.File_Descriptors;
